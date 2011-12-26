fbab0758d54c3ef630fcb8247586f4260a93de4d
Larry Reaves <larry@yrral.net>
2011-12-26 12:26:09 -0500

    add settings.rb (do not publish)

---
+++ settings.rb
@@ -0,0 +1,14 @@
+module BotSettings
+  KEY = 'key_goes_here'
+  SECRET = 'secret_goes_here'
+
+  DRY_RUN = false
+  DRY_BTC = 100
+  DRY_USD = 400
+
+  DECIMAL_PLACES = 5
+
+  ORDER_PAIRS = 3
+  ORDER_SIZE = 3
+  INTERVAL = 0.015
+end
